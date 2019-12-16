0be0a91d40d551834e8e58b34e3a852707e98e89
Eve Noirault <student@Makerss-MacBook-Air.local>
2019-12-16 16:33:32 +0000

    link user database with user class

--- lib/user.rb
+++ lib/user.rb
@@ -1,15 +1,27 @@
+require 'pg'
+
 class User
 
 attr_reader :username
 
-  def initialize(username:, email:, password:)
+  def initialize(username:, email:, password:, id:, created_at:)
     @username = username
     @email = email
     @password = password
   end
 
     def self.sign_up(username:, email:, password:)
-      User.new(username: username, email: email, password: password)
+      if ENV['RACK_ENV'] == 'test'
+        @connection = PG.connect(dbname: 'bnb_test')
+      else
+        @connection = PG.connect(dbname: "bnb")
+      end
+
+      result = @connection.exec("INSERT INTO users(username, email, password)
+        VALUES('#{username}', '#{email}', '#{password}')
+        RETURNING username, email, password, id, created_at;")
+
+      User.new(username: result[0]["username"], email: result[0]["email"], password: result[0]["password"], id: result[0]["id"], created_at: result[0]["created_at"])
     end
 
 end

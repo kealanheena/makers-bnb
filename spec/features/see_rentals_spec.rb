963f9dcacee2ca817eb5c4643d7f100b2625f805
kealanheena <kealanheena@gmail.com>
2019-12-16 17:23:51 +0000

    add rental class and all method

--- spec/features/see_rentals_spec.rb
+++ spec/features/see_rentals_spec.rb
@@ -3,7 +3,11 @@ feature "rentals list" do
   scenario "should see a list of rentals" do
     visit '/'
     expect(page).to have_content "Makers BnB"
-    click_on 'See Rentals'
+  end
+
+  scenario "should see a list of rentals" do
+    visit "/rentals"
+    
     expect(page).to have_content "Place 1"
     expect(page).to have_content "Place 2"
   end

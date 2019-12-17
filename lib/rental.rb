92aa6449dde8ee3b01401eb9b811d8ba1292a135
dbacall <dbacall@hotmail.co.uk>
2019-12-17 11:51:41 +0000

    add method for adding rentals to rental class

--- lib/rental.rb
+++ lib/rental.rb
@@ -1,18 +1,35 @@
 require 'pg'
 
 class Rental
-  attr_reader :name
+  attr_reader :name, :description, :price
   
-  def initialize(name)
+  def initialize(name, description, price)
     @name = name
+    @description = description
+    @price = price
   end
 
   def self.all
-    connection = PG.connect(dbname: 'bnb')
-    result = connection.exec('SELECT * FROM rentals;')
-
+    database_selector
+    result = @connection.exec('SELECT * FROM rentals;')
     result.map {|dwelling|
-      Rental.new(dwelling['name'])
+      Rental.new(dwelling['name'], dwelling['description'], dwelling['price'])
     }
+  end
+
+  def self.add(name, description, price)
+    database_selector
+    @connection.exec("INSERT INTO rentals(name, description, price)
+    VALUES('#{name}', '#{description}', '#{price}')")
+  end
+
+  private
+
+  def self.database_selector
+    if ENV['ENVIRONMENT'] = 'test'
+      @connection = PG.connect(dbname: 'bnb_test')
+    else
+      @connection = PG.connect(dbname: 'bnb')
+    end
   end
 end
---
+++ spec/web_helpers.rb
@@ -0,0 +1,4 @@
+def add_rentals
+  Rental.add('Place 1', 'nice place', '20')
+  Rental.add('Place 2', 'great place', '30')
+end

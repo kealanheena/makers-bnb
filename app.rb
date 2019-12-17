10155dd25b39807780e9719c7b57e413e730a94e
kealanheena <kealanheena@gmail.com>
2019-12-17 11:56:36 +0000

    add layout

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require 'sinatra/base'
 require './lib/rental.rb'
 
-class MakersBnB < Sinatra::Base 
+class MakersBnB < Sinatra::Base
 
   get '/' do
-    erb :index
+    erb :index, { :layout => :layout }
   end
   
   get '/rentals' do
     @list = Rental.all
-    erb :rentals
+    erb :rentals, { :layout => :layout }
   end
 
   run! if app_file == $0

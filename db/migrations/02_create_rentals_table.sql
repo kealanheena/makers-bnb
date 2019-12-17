7456b2a43d5908710155f7a2dee380ea1e985084
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-12-17 11:08:48 +0000

    Format migrations folder

--- db/migrations/02_create_rentals_table.sql
+++ db/migrations/02_create_rentals_table.sql
@@ -1,1 +1,3 @@
-CREATE TABLE rentals(id SERIAL PRIMARY KEY, name varchar(60), description varchar(250), price numeric(5, 2), created_at TIMESTAMP NOT NULL DEFAULT NOW(), user_id numeric);
+CREATE TABLE rentals(id SERIAL PRIMARY KEY, name varchar(60),
+  description varchar(250), price numeric(5, 2),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(), user_id numeric);

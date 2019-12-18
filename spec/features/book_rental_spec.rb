a92beb19b21ed215c472ae0f13f098f314e822a5
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-12-18 22:29:51 +0000

    Add feature test for check dates feature & hide book button unless date is available

--- spec/features/book_rental_spec.rb
+++ spec/features/book_rental_spec.rb
@@ -5,8 +5,10 @@ feature "booking" do
     add_rental_feature
 
     click_on "Place 1"
-    #will add date selection later
+    fill_in "date", with: '2019-05-04'
+    click_on "Check Date"
     click_on "Book"
+
     text = "Your booking request has been sent and is now pending approval."
     expect(page).to have_content text
   end
--- spec/features/check_dates_spec.rb
+++ spec/features/check_dates_spec.rb
@@ -1,9 +1,11 @@
 feature 'checking dates' do
 
-  scenario 'it should check if a dates availabe to rent a property' do
+  before do
     sign_up_test
     add_rental_feature
+  end
 
+  scenario 'it should show that a date is available' do
     click_on 'Place 1'
     fill_in 'date', with: '2019-05-04'
     click_on 'Check Date'
@@ -11,4 +13,11 @@ feature 'checking dates' do
     expect(page).to have_content 'Date available!'
   end
 
+  scenario 'it should show that a date is available' do
+    click_on 'Place 1'
+    fill_in 'date', with: '2020-12-01'
+    click_on 'Check Date'
+
+    expect(page).to have_content 'Not available'
+  end
 end

46b50b4c96df909d8ad1e793c71cd16b04d2cfc4
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-12-18 19:27:29 +0000

    Refactor user class and unit tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,44 +3,46 @@ require 'user'
 describe "User" do
 
   before do
-    @jd = sign_up_user
+    @user = sign_up_user
   end
 
   describe ".sign_up" do
     it "can sign up and create a new user" do
 
-      expect(@jd).to be_instance_of User
-      expect(@jd.username).to eq("JDTest")
+      expect(@user).to be_instance_of User
+      expect(@user.username).to eq("JDTest")
     end
 
     it "fails to sign up if email already has an account" do
-      expect(User.sign_up(username: "helloBob", email: "jd@test.com", password: "por85g")).to eq :email_clash
+      user = User.sign_up(username: "helloBob", email: "jd@test.com", password: "por85g")
+      expect(user).to eq :email_clash
     end
 
     it "fails to sign up if username already has an account" do
-      expect(User.sign_up(username: "JDTest", email: "hello@test.com", password: "super87")).to eq :username_clash
+      user = User.sign_up(username: "JDTest", email: "hello@test.com", password: "super87")
+      expect(user).to eq :username_clash
     end
   end
 
   describe ".authenticate" do
     it "can log in an existing user" do
 
-      jd = User.authenticate(email: "jd@test.com", password: "por85g" )
-      expect(jd).to be_instance_of User
-      expect(jd.username).to eq("JDTest")
+      user = User.authenticate(email: "jd@test.com", password: "por85g" )
+      expect(user).to be_instance_of User
+      expect(user.username).to eq("JDTest")
     end
 
-     context "fails to log in if user is not recognized" do
+    context "fails to log in if user is not recognized" do
       it "fails if wrong email" do
-        expect(User.authenticate(email: "jc@taste.com", password: "por85g" )).to be false
+        user = User.authenticate(email: "jc@taste.com", password: "por85g" )
+        expect(user).to be false
       end
 
       it "fails if wrong password" do
-        connection = PG.connect(dbname: 'bnb_test')
-        connection.exec("TRUNCATE TABLE rentals, users, bookings;")
-        expect(User.authenticate(email: "jd@test.com", password: "proc85g" )).to be false
-      end
-     end
 
+        user = User.authenticate(email: "jd@test.com", password: "proc85g" )
+        expect(user).to be false
+      end
+    end
   end
 end
